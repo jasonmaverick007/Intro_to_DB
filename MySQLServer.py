e72f4201e24b3cf68809c3a12097cfdb8da95823
Jason <jasonretro580@gmail.com>
2025-06-27 22:28:25 +0300

    Added missing parts of the code to MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -12,7 +12,7 @@ try:
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database'alx_book_store' created successfully!")
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error connecting to MySQL: {e}")
 
 finally:
